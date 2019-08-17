0118e108c9fac6bf464431f19fc726c98dac651b
Pumper Knickle <pumperknickle@protonmail.com>
2019-08-17 08:56:04 -0700

    sqlite conformance

--- Sources/MerkleFlowModels/Implementations/AuthenticatedADRModel256.swift
+++ Sources/MerkleFlowModels/Implementations/AuthenticatedADRModel256.swift
@@ -1,17 +1,15 @@
 import Foundation
 import CryptoStarterPack
-import Fluent
+import FluentSQLite
 
-public struct AuthenticatedADRModel256<D>: Model where D: QuerySupporting {
-    public typealias Database = D
-    public typealias ID = UInt256
+public struct AuthenticatedADRModel256: SQLiteModel {
+    public typealias ID = Int
     public typealias AuthADR = AuthenticatedADR256
     public typealias ADR = AuthADR.ADR
     public typealias Digest = AuthADR.Digest
     public typealias Signature = AuthADR.Signature
-    public static var idKey: IDKey { return \._id }
 
-    private var _id: ID?
+    public var id: ID?
     private let _timestamp: Double!
     private let _agent: Digest!
     private let _deployer: Digest!
@@ -22,7 +20,7 @@ public struct AuthenticatedADRModel256<D>: Model where D: QuerySupporting {
     public var authADR: AuthADR! { return AuthADR(adr: adr, deployerSignature: _deployerSignature) }
     
     public init(id: ID? = nil, timestamp: Double = Date().timeIntervalSince1970, authAdr: AuthADR) {
-        self._id = id
+        self.id = id
         self._timestamp = timestamp
         self._agent = authAdr.adr.agent
         self._deployer = authAdr.adr.deployer
